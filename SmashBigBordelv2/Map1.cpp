a68ea6c7391276db918f78fc7b6418e30a706097
AlexLuthor09 <capalex@hotmail.be>
2020-05-11 11:41:57 +0200

    les différentes palteformes

--- SmashBigBordelv2/Map1.cpp
+++ SmashBigBordelv2/Map1.cpp
@@ -17,42 +17,32 @@ void Map1::setPlatefomes()
 	this->getPlatform(0)->setTexture(*t->SetTexture(this->GetPath()));
 	this->getPlatform(0)->setTextureRect(sf::IntRect(0, 0, 100, 20));
 
-	/*
-	this->getPlatform(1)->setPosition(0, 0);
-	this->getPlatform(1)->setLargeur(100);
-	this->getPlatform(1)->setHauteur(20);
+	
+	this->getPlatform(1)->setPosition(100, 0);
 	this->getPlatform(1)->setTexture(*t->SetTexture(this->GetPath()));
 	this->getPlatform(1)->setTextureRect(sf::IntRect(0, 0, 100, 20));
 
 
-	this->getPlatform(2)->setPosition(0, 0);
-	this->getPlatform(2)->setLargeur(100);
-	this->getPlatform(2)->setHauteur(20);
+	this->getPlatform(2)->setPosition(-100, 0);
 	this->getPlatform(2)->setTexture(*t->SetTexture(this->GetPath()));
 	this->getPlatform(2)->setTextureRect(sf::IntRect(0, 0, 100, 20));
 
 
-	this->getPlatform(3)->setPosition(0, 0);
-	this->getPlatform(3)->setLargeur(100);
-	this->getPlatform(3)->setHauteur(20);
+	this->getPlatform(3)->setPosition(50, 20);
 	this->getPlatform(3)->setTexture(*t->SetTexture(this->GetPath()));
 	this->getPlatform(3)->setTextureRect(sf::IntRect(0, 0, 100, 20));
 
 
-	this->getPlatform(4)->setPosition(0, 0);
-	this->getPlatform(4)->setLargeur(100);
-	this->getPlatform(4)->setHauteur(20);
+	this->getPlatform(4)->setPosition(-500, 200);
 	this->getPlatform(4)->setTexture(*t->SetTexture(this->GetPath()));
-	this->getPlatform(4)->setTextureRect(sf::IntRect(0, 0, 100, 20));
+	this->getPlatform(4)->setTextureRect(sf::IntRect(0, 0, 1000, 20));
 
 
-	this->getPlatform(5)->setPosition(0, 0);
-	this->getPlatform(5)->setLargeur(100);
-	this->getPlatform(5)->setHauteur(20);
+	this->getPlatform(5)->setPosition(60, 0);
 	this->getPlatform(5)->setTexture(*t->SetTexture(this->GetPath()));
 	this->getPlatform(5)->setTextureRect(sf::IntRect(0, 0, 100, 20));
 
-	*/
+	
 }
 std::string Map1::GetPath()
 {
