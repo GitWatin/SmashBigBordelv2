fdc1c344b2a4d02122068e5ea9a11ed84d6ec486
Watin <valentde@hotmail.com>
2020-05-11 17:19:53 +0200

    test

--- SmashBigBordelv2/Map.h
+++ SmashBigBordelv2/Map.h
@@ -4,19 +4,20 @@
 #include <iostream>
 class Map
 {
-    public:
+public:
 
-    Map(int plateforme);
+	Map(int plateforme);
 	~Map(void);
 	virtual void setBackground() = 0;
-	virtual void setPlatefomes()= 0;
+	virtual void setPlatefomes() = 0;
 	int countPlatforms();
 	Plateforme* getPlatform(int pos);
 	virtual std::vector< Plateforme* >GetVectorPlatefomes();
 	Back_Ground* getBackground();
-	
+
 protected:
 
 	std::vector < Plateforme* > plateformes;
 	Back_Ground *BackGround;
 
+};
