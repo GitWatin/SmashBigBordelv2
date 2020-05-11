e707871dd69d2d98e6676f5c42ce0e547c0ec878
Watin <valentde@hotmail.com>
2020-05-11 15:28:52 +0200

    Texture add - VDEN

--- SmashBigBordelv2/Map1.h
+++ SmashBigBordelv2/Map1.h
@@ -10,18 +10,15 @@ class Map1 :
 		Map1();
 		virtual void setPlatefomes();
 		virtual void setBackground();
-
 		virtual std::string GetPath(int taille);
-
 		std::string GetPathBackground();
-		//virtual std::string GetPathPlateforme();
-		//virtual std::string GetPathBackground();
+
 	protected:
 		
-		//std::string PathPlateformePetite = \\Textures\\background.png
-		std::string PathPlateformeTiny = "D:\\projet_cpp\\SmashBigBordelv2\\SmashBigBordelv2\\Textures\\plat_tiny.png";
-		std::string PathPlateformePetite = "D:\\projet_cpp\\SmashBigBordelv2\\SmashBigBordelv2\\Textures\\plat_petite.png";
-		std::string PathPlateformeGrande = "D:\\projet_cpp\\SmashBigBordelv2\\SmashBigBordelv2\\Textures\\plat_grande.png";
-		std::string PathBackground = "D:\\projet_cpp\\SmashBigBordelv2\\SmashBigBordelv2\\Textures\\background.png";
+		std::string PathPlateformeTiny = "plat_tiny.png";
+		std::string PathPlateformePetite = "plat_petite.png";
+		std::string PathPlateformeGrande = "plat_grande.png";
+		std::string PathBackground = "background.png";
 };
+
 
