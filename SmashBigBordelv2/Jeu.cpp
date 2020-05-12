52e7b6b10f2cd324f4a845a5d731b94fac613f1d
AlexLuthor09 <capalex@hotmail.be>
2020-05-12 11:42:29 +0200

    tg

--- SmashBigBordelv2/Jeu.cpp
+++ SmashBigBordelv2/Jeu.cpp
@@ -8,9 +8,6 @@ Jeu::Jeu()
 	this->fenetre = new sf::RenderWindow(sf::VideoMode(1600, 900), "SmashBigBordel", sf::Style::Titlebar | sf::Style::Close, sf::ContextSettings(32));
 	this->fenetre->setFramerateLimit(60);
 	this->clock = new sf::Clock;
-
-	// Timer du jeu (5min)
-	this->clock_jeu = new sf::Clock;
 	this->dureeIteration = sf::Time::Zero;
 }
 
@@ -37,9 +34,18 @@ sf::RenderWindow* Jeu::GetWindow()
 
 void Jeu::CheckInput(sf::Event event)
 {
-	switch (event.key.code)
+	switch (event.type)
 	{
-		de
+	case sf::Event::Closed:
+		fenetre->close();
+		break;
+	case sf::Event::EventType::KeyPressed:
+		switch (event.key.code)
+		{
+		case sf::Keyboard::Right:
+			
+			break;
+		}
 		break;
 	}
 }
