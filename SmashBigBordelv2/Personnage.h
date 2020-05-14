4bc00cd7a2e94188bbbbf5c0738f3efd5ff30f4c
AlexLuthor09 <capalex@hotmail.be>
2020-05-14 16:33:16 +0200

    attaque

--- SmashBigBordelv2/Personnage.h
+++ SmashBigBordelv2/Personnage.h
@@ -9,6 +9,7 @@ class Personnage :
     public:
 	Personnage();
 	~Personnage();
+
 	virtual void Attaque(Personnage *NomCible, int TypeAttaque) = 0;
 	virtual void SetPourcentages(int percent);
 	virtual int GetPourcentages();
